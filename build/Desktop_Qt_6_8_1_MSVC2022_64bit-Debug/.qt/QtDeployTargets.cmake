f483ac818e2d0b940fada966ddf73af08ee484ef
Ye-Yu-Mo <2908156919@qq.com>
2024-12-25 20:54:43 +0800

    init

---
+++ build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_XuChat-client_FILE D:/QtPro/XuChat-client/build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/XuChat-client.exe)
+set(__QT_DEPLOY_TARGET_XuChat-client_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_XuChat-client_RUNTIME_DLLS D:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
